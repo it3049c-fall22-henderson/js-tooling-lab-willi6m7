df301a7f4af17e566aed83990931e41c6c845e27
willi6m7 <willi6m7@mail.uc.edu>
2022-11-05 15:38:10 -0400

    linting and transpiling

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "array-bracket-spacing": [2, "always"],
+        "no-const-assign": 2,
+        "no-var": "error",
+        "indent": [2, 2],
+        "quotes": [2, "backtick"],
+        "eqeqeq": "error"
+    }
+}

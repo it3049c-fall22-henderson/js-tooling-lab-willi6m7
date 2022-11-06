526b32415c587e44e305a38eecd909f95ab769b6
willi6m7 <willi6m7@mail.uc.edu>
2022-11-06 15:19:50 -0500

    jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "array-bracket-spacing": [2, "always"],
-        "no-const-assign": 2,
-        "no-var": "error",
-        "indent": [2, 2],
-        "quotes": [2, "backtick"],
-        "eqeqeq": "error"
-    }
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jest": true
+  },
+  "extends": [
+    `eslint:recommended`,
+    `plugin:react/recommended`
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": `latest`,
+    "sourceType": `module`
+  },
+  "plugins": [
+    `react`
+  ],
+  "rules": {
+    "array-bracket-spacing": [ 2, `always` ],
+    "no-const-assign": 2,
+    "no-var": `error`,
+    "indent": [ 2, 2 ],
+    "quotes": [ 2, `backtick` ],
+    "eqeqeq": `error`
+  }
 }
